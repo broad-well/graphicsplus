2eb8df5053d12f4b4384b6ca2dd954ec79f30f26
broad-well <michaelpeng2002@icloud.com>
2016-08-07 22:36:03 -0700

    light documentation & compression

--- graphicsplus.py
+++ graphicsplus.py
@@ -1,10 +1,26 @@
 # Michael's GraphicsPlus module
 
 from graphics import *
-import json
-gplus_debug = False
+import json, lzma
+
+gplus_options = {
+    "debug": False,
+    "compress": True
+}
+
+def gplus_attribute(key, value="ATTRIBUTE_NULL_VALUE"):
+    """Changes GraphicsPlus options.
+    When called with 1 argument, this function returns the value of the requested option.
+    When called with 2 arguments, this function sets the value of the requested option and returns the set value.
+    May raise KeyError if the key is not present and the function was called with 1 argument."""
+    if value == "ATTRIBUTE_NULL_VALUE":
+        return gplus_options[key]
+    else:
+        gplus_options[key] = value
+        return gplus_options[key]
 
 def gplus_get_center(obj):
+    """Reserved for GraphicsPlus internal use"""
     # Returns the centroid of a given shape (Rectangle, Circle, Oval, Line, Polygon, Image).
     if isinstance(obj, Polygon):
         xs = [a.x for a in obj.getPoints()]
@@ -18,16 +34,20 @@ def gplus_get_center(obj):
         raise RuntimeError("Argument type not supported")
 
 def gplus_relation(p1, p2):
+    """Reserved for GraphicsPlus internal use"""
     # p1 in gplus_relation to p2
     return (p1.x-p2.x, p1.y-p2.y)
 
 def gplus_vector_apply(center, rel):
+    """Reserved for GraphicsPlus internal use"""
     return Point(center.x+rel.x, center.y+rel.y)
 
 def gplus_invert_point(p):
+    """Reserved for GraphicsPlus internal use"""
     return Point(0-p.x, 0-p.y)
 
 def gplus_dump_point(p):
+    """Reserved for GraphicsPlus internal use"""
     assert type(p) == Point
     return json.dumps({
         "type":"Point",
@@ -37,6 +57,7 @@ def gplus_dump_point(p):
     })
 
 def gplus_parse_point(s):
+    """Reserved for GraphicsPlus internal use"""
     parsed = json.loads(s)
     assert parsed["type"] == "Point"
     p = Point(parsed["x"], parsed["y"])
@@ -44,6 +65,7 @@ def gplus_parse_point(s):
     return p
 
 def gplus_dump_poly(obj):
+    """Reserved for GraphicsPlus internal use"""
     assert type(obj) == Polygon
     attrs = {
         "type":"Polygon",
@@ -56,6 +78,7 @@ def gplus_dump_poly(obj):
     return json.dumps(attrs)
 
 def gplus_parse_poly(s, c):
+    """Reserved for GraphicsPlus internal use"""
     parsed = json.loads(s)
     assert parsed["type"] == "Polygon"
     points = [gplus_parse_point(a_) for a_ in parsed["points"]]
@@ -64,6 +87,7 @@ def gplus_parse_poly(s, c):
     return poly
 
 def gplus_dump_line(line):
+    """Reserved for GraphicsPlus internal use"""
     assert type(line) == Line
     attrs = {
         "type":"Line",
@@ -74,6 +98,7 @@ def gplus_dump_line(line):
     return json.dumps(attrs)
 
 def gplus_parse_line(s, c):
+    """Reserved for GraphicsPlus internal use"""
     parsed = json.loads(s)
     assert parsed["type"] == "Line"
     p1 = gplus_vector_apply(c, gplus_parse_point(parsed["offset"]))
@@ -88,26 +113,45 @@ gplus_types = {
 }
 
 def gplus_seek_types(objtype):
+    """Reserved for GraphicsPlus internal use"""
     for key in gplus_types.keys():
         if (type(objtype) == type and key == objtype.__name__) or (type(objtype) == str and json.loads(objtype)["type"] == key):
             return gplus_types[key]
     return None
 
+def gplus_get_fileresources(write):
+    """Reserved for GraphicsPlus internal use"""
+    if gplus_options["compress"]:
+        if write:
+            return (lzma.open, "x")
+        else:
+            return (lzma.open, "r")
+    else:
+        if write:
+            return (open, "w+b")
+        else:
+            return (open, "rb")
+
 def export_graphics(obj, filename):
+    """Exports the given graphical object to the specified filename."""
     # Exports the given object (shapes: Rectangle, Circle, Oval, Line, Polygon) and GraphWin.
     parsetuple = gplus_seek_types(type(obj))
     if parsetuple == None:
         print("export_graphics failure: {} objects are unsupported".format(type(obj).__name__))
         return False
     else:
-        with open(filename, "w+") as fil:
-            fil.write(parsetuple[0](obj))
+        fileres = gplus_get_fileresources(True)
+        with fileres[0](filename, fileres[1]) as fil:
+            fil.write(parsetuple[0](obj).encode())
         return True
 
 def import_graphics(filename, anchorpoint):
+    """Reads the file with the filename argument and returns the graphical object that the file contents represent.
+    Supply anchorpoint for all objects other than GraphWin for the coordinates (location) of the returning graphical object."""
     c = None
-    with open(filename) as fil:
-        c = fil.read()
+    fileres = gplus_get_fileresources(False)
+    with fileres[0](filename, fileres[1]) as fil:
+        c = fil.read().decode()
     parsetuple = gplus_seek_types(c)
     if parsetuple == None:
         print("import_graphics error: file not supported")
@@ -118,6 +162,7 @@ def import_graphics(filename, anchorpoint):
 # Window800;800;462%163.25->Polygon121.14%25.25|14.24%-36|;
 
 def gplus_dump_window(w):
+    """Reserved for GraphicsPlus internal use"""
     attrs = {
         "type":"GraphWin",
         "width":w.width,
@@ -129,11 +174,12 @@ def gplus_dump_window(w):
         if fun == None: raise RuntimeError("{} graphics object not supported".format(type(i).__name__))
         attrs["objects"].append({"center":gplus_dump_point(gplus_get_center(i)), "object":fun[0](i)})
     assert len(attrs["objects"]) == len(w.items)
-    if gplus_debug:
+    if gplus_options["debug"]:
         print("Debug: " + str(attrs))
     return json.dumps(attrs)
 
 def gplus_parse_window(s, title="Graphics Window"):
+    """Reserved for GraphicsPlus internal use"""
     parsed = json.loads(s)
     assert parsed["type"] == "GraphWin"
     win = GraphWin(title, parsed["width"], parsed["height"])
